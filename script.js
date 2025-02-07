81876324bd517444697750ac8e55926f386b8488
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-07 12:20:22 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+document.addEventListener("DOMContentLoaded", function () {
+	const modal = document.getElementById("modal");
+	const openModalButton = document.getElementById("openModal");
+	const closeButton = document.querySelector(".closebutton");
+
+	openModalButton.addEventListener('click', function () {
+		modal.style.display = "flex";
+	});
+
+	closeButton.addEventListener('click', function () {
+		modal.style.display = "none";
+	});
+
+	window.addEventListener('click', function (event) {
+		if (event.target === modal) {
+			modal.style.display = "none";
+		}
+	});
+
+});
